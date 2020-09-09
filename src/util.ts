b2b8d2dc1636a1af81aad38f3dabd3677a482ef6
Xearty <xeartybg@gmail.com>
2020-09-09 19:37:35 +0300

    Now drawing the board in one console.log call

--- src/util.ts
+++ src/util.ts
@@ -16,31 +16,30 @@ export function displayBoard(board: number[][], initialCells: boolean[][]): void
     const digitColor = '\x1b[31m';
     const resetColor = '\x1b[0m';
     const bright = '\x1b[1m';
-    const  initialCellsColor = '\x1b[33m'
-    console.log('╭-------┬-------┬-------╮');
+    const initialCellsColor = '\x1b[33m'
+
+    let boardStr = '╭-------┬-------┬-------╮\n';
     for (let row = 0; row < board.length; row++) {
-        let line = '';
         if (row % 3 == 0 && row != 0)
-            console.log('├-------┼-------┼-------┤');
+            boardStr += '├-------┼-------┼-------┤\n';
 
         for (let col = 0; col < board.length; col++) {
-            if (col % 3 == 0) line += '| ';
+            if (col % 3 == 0)
+                boardStr += '| ';
             if (board[row][col] === 0)
-                line += '·';
+                boardStr += '·';
             else if (initialCells[row][col])
-                line += `${bright}${initialCellsColor}${board[row][col]}${resetColor}`;
+                boardStr += `${bright}${initialCellsColor}${board[row][col]}${resetColor}`;
             else
-                line += `${bright}${digitColor}${board[row][col]}${resetColor}`;
+                boardStr += `${bright}${digitColor}${board[row][col]}${resetColor}`;
 
-            line += ' ';
+            boardStr += ' ';
         }
 
-        line += '|';
-
-        console.log(line);
-
+        boardStr += '|\n';
     }
-    console.log('╰-------┴-------┴-------╯');
+    boardStr += '╰-------┴-------┴-------╯';
+    console.log(boardStr);
 }
 
 export function copy(board: number[][]): number[][] {
